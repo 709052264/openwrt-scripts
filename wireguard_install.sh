12fa736e7c0ab6765f9b74275782177bfaf7bc73
c040656 <91834487+c040656@users.noreply.github.com>
2023-06-09 18:23:21 -0400

    Update wireguard_install.sh

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -9,9 +9,8 @@ opkg install wireguard-tools
  
 # Configuration parameters
 WG_IF="wg0"
-WG_PORT="51820"
+WG_PORT="51236"
 WG_ADDR="192.168.9.1/24"
-WG_ADDR6="fdf1:7610:d152:3a9c::1/64"
 
 # Generate and exchange the keys
 umask u=rw,g=,o=
@@ -45,7 +44,6 @@ uci set network.${WG_IF}.proto="wireguard"
 uci set network.${WG_IF}.private_key="${WG_KEY}"
 uci set network.${WG_IF}.listen_port="${WG_PORT}"
 uci add_list network.${WG_IF}.addresses="${WG_ADDR}"
-uci add_list network.${WG_IF}.addresses="${WG_ADDR6}"
  
 # Add VPN peers
 uci -q delete network.wgclient
@@ -53,7 +51,6 @@ uci set network.wgclient="wireguard_${WG_IF}"
 uci set network.wgclient.public_key="${WG_PUB}"
 uci set network.wgclient.preshared_key="${WG_PSK}"
 uci add_list network.wgclient.allowed_ips="${WG_ADDR%.*}.0/${WG_ADDR#*/}"
-uci add_list network.wgclient.allowed_ips="${WG_ADDR6%/*}/${WG_ADDR6#*/}"
 uci commit network
 /etc/init.d/network restart
 
